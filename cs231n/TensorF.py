3fd8ff951a08e3eee6d91612b0393ebd84494c6c
Zeyuan Zhang <ge34caj@mytum.de>
2019-03-26 15:26:23 +0100

    update TensorFlow file

---
+++ cs231n/TensorF.py
@@ -0,0 +1,324 @@
+import os
+import tensorflow as tf
+import numpy as np
+import math
+import timeit
+import matplotlib.pyplot as plt
+from ResNet import resnet_110
+
+def load_cifar10(num_training=49000, num_validation=1000, num_test=10000):
+    """
+    Fetch the CIFAR-10 dataset from the web and perform preprocessing to prepare
+    it for the two-layer neural net classifier. These are the same steps as
+    we used for the SVM, but condensed to a single function.
+    """
+    # Load the raw CIFAR-10 dataset and use appropriate data types and shapes
+    cifar10 = tf.keras.datasets.cifar10.load_data()
+    (X_train, y_train), (X_test, y_test) = cifar10
+    X_train = np.asarray(X_train, dtype=np.float32)
+    y_train = np.asarray(y_train, dtype=np.int32).flatten()
+    X_test = np.asarray(X_test, dtype=np.float32)
+    y_test = np.asarray(y_test, dtype=np.int32).flatten()
+
+    # Subsample the data
+    mask = range(num_training, num_training + num_validation)
+    X_val = X_train[mask]
+    y_val = y_train[mask]
+    mask = range(num_training)
+    X_train = X_train[mask]
+    y_train = y_train[mask]
+    mask = range(num_test)
+    X_test = X_test[mask]
+    y_test = y_test[mask]
+
+    # Normalize the data: subtract the mean pixel and divide by std
+    mean_pixel = X_train.mean(axis=(0, 1, 2), keepdims=True)
+    std_pixel = X_train.std(axis=(0, 1, 2), keepdims=True)
+    X_train = (X_train - mean_pixel) / std_pixel
+    X_val = (X_val - mean_pixel) / std_pixel
+    X_test = (X_test - mean_pixel) / std_pixel
+
+    return X_train, y_train, X_val, y_val, X_test, y_test
+
+
+# Invoke the above function to get our data.
+NHW = (0, 1, 2)
+X_train, y_train, X_val, y_val, X_test, y_test = load_cifar10()
+print('Train data shape: ', X_train.shape)
+print('Train labels shape: ', y_train.shape, y_train.dtype)
+print('Validation data shape: ', X_val.shape)
+print('Validation labels shape: ', y_val.shape)
+print('Test data shape: ', X_test.shape)
+print('Test labels shape: ', y_test.shape)
+
+
+class Dataset(object):
+    def __init__(self, X, y, batch_size, shuffle=False):
+        """
+        Construct a Dataset object to iterate over data X and labels y
+
+        Inputs:
+        - X: Numpy array of data, of any shape
+        - y: Numpy array of labels, of any shape but with y.shape[0] == X.shape[0]
+        - batch_size: Integer giving number of elements per minibatch
+        - shuffle: (optional) Boolean, whether to shuffle the data on each epoch
+        """
+        assert X.shape[0] == y.shape[0], 'Got different numbers of data and labels'
+        self.X, self.y = X, y
+        self.batch_size, self.shuffle = batch_size, shuffle
+
+    def __iter__(self):
+        N, B = self.X.shape[0], self.batch_size
+        idxs = np.arange(N)
+        if self.shuffle:
+            np.random.shuffle(idxs)
+        return iter((self.X[i:i + B], self.y[i:i + B]) for i in range(0, N, B))
+
+
+train_dset = Dataset(X_train, y_train, batch_size=64, shuffle=True)
+val_dset = Dataset(X_val, y_val, batch_size=64, shuffle=False)
+test_dset = Dataset(X_test, y_test, batch_size=64)
+
+
+# Set up some global variables
+USE_GPU = True
+
+if USE_GPU:
+    device = '/device:GPU:0'
+else:
+    device = '/cpu:0'
+
+# Constant to control how often we print when training models
+print_every = 100
+
+print('Using device: ', device)
+
+
+def check_accuracy(sess, dset, x, scores, is_training=None):
+    """
+    Check accuracy on a classification model.
+
+    Inputs:
+    - sess: A TensorFlow Session that will be used to run the graph
+    - dset: A Dataset object on which to check accuracy
+    - x: A TensorFlow placeholder Tensor where input images should be fed
+    - scores: A TensorFlow Tensor representing the scores output from the
+      model; this is the Tensor we will ask TensorFlow to evaluate.
+
+    Returns: Nothing, but prints the accuracy of the model
+    """
+    num_correct, num_samples = 0, 0
+    for x_batch, y_batch in dset:
+        feed_dict = {x: x_batch, is_training: 0}
+        scores_np = sess.run(scores, feed_dict=feed_dict)
+        y_pred = scores_np.argmax(axis=1)
+        num_samples += x_batch.shape[0]
+        num_correct += (y_pred == y_batch).sum()
+    acc = float(num_correct) / num_samples
+    print('Got %d / %d correct (%.2f%%)' % (num_correct, num_samples, 100 * acc))
+
+
+def train_part34(model_init_fn, optimizer_init_fn, num_epochs=1):
+    """
+    Simple training loop for use with models defined using tf.keras. It trains
+    a model for one epoch on the CIFAR-10 training set and periodically checks
+    accuracy on the CIFAR-10 validation set.
+
+    Inputs:
+    - model_init_fn: A function that takes no parameters; when called it
+      constructs the model we want to train: model = model_init_fn()
+    - optimizer_init_fn: A function which takes no parameters; when called it
+      constructs the Optimizer object we will use to optimize the model:
+      optimizer = optimizer_init_fn()
+    - num_epochs: The number of epochs to train for
+
+    Returns: Nothing, but prints progress during trainingn
+    """
+    print(device)
+    tf.reset_default_graph()
+    with tf.device(device):
+        # Construct the computational graph we will use to train the model. We
+        # use the model_init_fn to construct the model, declare placeholders for
+        # the data and labels
+        x = tf.placeholder(tf.float32, [None, 32, 32, 3])
+        y = tf.placeholder(tf.int32, [None])
+
+        # We need a place holder to explicitly specify if the model is in the training
+        # phase or not. This is because a number of layers behaves differently in
+        # training and in testing, e.g., dropout and batch normalization.
+        # We pass this variable to the computation graph through feed_dict as shown below.
+        is_training = tf.placeholder(tf.bool, name='is_training')
+
+        # Use the model function to build the forward pass.
+        scores = model_init_fn(x, is_training)
+
+        # Compute the loss like we did in Part II
+        loss = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=y, logits=scores)
+        loss = tf.reduce_mean(loss)
+
+        # Use the optimizer_fn to construct an Optimizer, then use the optimizer
+        # to set up the training step. Asking TensorFlow to evaluate the
+        # train_op returned by optimizer.minimize(loss) will cause us to make a
+        # single update step using the current minibatch of data.
+
+        # Note that we use tf.control_dependencies to force the model to run
+        # the tf.GraphKeys.UPDATE_OPS at each training step. tf.GraphKeys.UPDATE_OPS
+        # holds the operators that update the states of the network.
+        # For example, the tf.layers.batch_normalization function adds the running mean
+        # and variance update operators to tf.GraphKeys.UPDATE_OPS.
+        optimizer = optimizer_init_fn()
+        update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
+        with tf.control_dependencies(update_ops):
+            train_op = optimizer.minimize(loss)
+
+    # Now we can run the computational graph many times to train the model.
+    # When we call sess.run we ask it to evaluate train_op, which causes the
+    # model to update.
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        t = 0
+        for epoch in range(num_epochs):
+            print('Starting epoch %d' % epoch)
+            for x_np, y_np in train_dset:
+                feed_dict = {x: x_np, y: y_np, is_training: 1}
+                loss_np, _ = sess.run([loss, train_op], feed_dict=feed_dict)
+                if t % print_every == 0:
+                    print('Iteration %d, loss = %.4f' % (t, loss_np))
+                    check_accuracy(sess, val_dset, x, scores, is_training=is_training)
+                    print()
+                t += 1
+
+
+def test_part34(model_init_fn, optimizer_init_fn, num_epochs=1):
+    """
+    Simple training loop for use with models defined using tf.keras. It trains
+    a model for one epoch on the CIFAR-10 training set and periodically checks
+    accuracy on the CIFAR-10 validation set.
+
+    Inputs:
+    - model_init_fn: A function that takes no parameters; when called it
+      constructs the model we want to train: model = model_init_fn()
+    - optimizer_init_fn: A function which takes no parameters; when called it
+      constructs the Optimizer object we will use to optimize the model:
+      optimizer = optimizer_init_fn()
+    - num_epochs: The number of epochs to train for
+
+    Returns: Nothing, but prints progress during trainingn
+    """
+    print(device)
+    tf.reset_default_graph()
+    with tf.device(device):
+        # Construct the computational graph we will use to train the model. We
+        # use the model_init_fn to construct the model, declare placeholders for
+        # the data and labels
+        x = tf.placeholder(tf.float32, [None, 32, 32, 3])
+        y = tf.placeholder(tf.int32, [None])
+
+        # We need a place holder to explicitly specify if the model is in the training
+        # phase or not. This is because a number of layers behaves differently in
+        # training and in testing, e.g., dropout and batch normalization.
+        # We pass this variable to the computation graph through feed_dict as shown below.
+        is_training = tf.placeholder(tf.bool, name='is_training')
+
+        # Use the model function to build the forward pass.
+        scores = model_init_fn(x, is_training)
+
+        # Compute the loss like we did in Part II
+        loss = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=y, logits=scores) + tf.losses.get_regularization_loss()
+        loss = tf.reduce_mean(loss)
+
+        # Use the optimizer_fn to construct an Optimizer, then use the optimizer
+        # to set up the training step. Asking TensorFlow to evaluate the
+        # train_op returned by optimizer.minimize(loss) will cause us to make a
+        # single update step using the current minibatch of data.
+
+        # Note that we use tf.control_dependencies to force the model to run
+        # the tf.GraphKeys.UPDATE_OPS at each training step. tf.GraphKeys.UPDATE_OPS
+        # holds the operators that update the states of the network.
+        # For example, the tf.layers.batch_normalization function adds the running mean
+        # and variance update operators to tf.GraphKeys.UPDATE_OPS.
+        optimizer = optimizer_init_fn()
+        update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
+        with tf.control_dependencies(update_ops):
+            train_op = optimizer.minimize(loss)
+
+    # Now we can run the computational graph many times to train the model.
+    # When we call sess.run we ask it to evaluate train_op, which causes the
+    # model to update.
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        t = 0
+        for epoch in range(num_epochs):
+            print('Starting epoch %d' % epoch)
+            for x_np, y_np in train_dset:
+                feed_dict = {x: x_np, y: y_np, is_training: 0}
+                loss_np, _ = sess.run([loss, train_op], feed_dict=feed_dict)
+                if t % print_every == 0:
+                    print('Iteration %d, loss = %.4f' % (t, loss_np))
+                    check_accuracy(sess, test_dset, x, scores, is_training=0)
+                    print()
+                t += 1
+
+def model_init_fn(inputs, is_training):
+    model = None
+    ############################################################################
+    # TODO: Construct a model that performs well on CIFAR-10                   #
+    ############################################################################
+    input_shape = (32, 32, 3)
+    initializer = tf.variance_scaling_initializer(scale=2.0)
+    conv1 = tf.layers.conv2d(inputs, 128, 3, 1, kernel_initializer= initializer)
+    ba1 = tf.layers.batch_normalization(conv1, training = is_training)
+    ba1 = tf.nn.relu(ba1)
+    conv2 = tf.layers.conv2d(ba1, 256, 3, kernel_initializer= initializer)
+    ba2 = tf.layers.batch_normalization(conv2, training = is_training)
+    ba2 = tf.nn.relu(ba2)
+    pool1 = tf.layers.max_pooling2d(ba2, 2, 2)
+
+    conv3 = tf.layers.conv2d(pool1, 512, 3, kernel_initializer= initializer)
+    ba3 = tf.layers.batch_normalization(conv3, training= is_training)
+    ba3 = tf.nn.relu(ba3)
+    conv4 = tf.layers.conv2d(ba3, 256, 3, kernel_initializer= initializer)
+    ba4 = tf.layers.batch_normalization(conv4, training = is_training)
+    ba4 = tf.nn.relu(ba4)
+    pool2 = tf.layers.max_pooling2d(ba4, 2, 2)
+
+    pool2_flat = tf.reshape(pool2, [-1, 5*5*256])
+    dense1 = tf.layers.dense(pool2_flat, units = 512, activation = tf.nn.relu)
+    ba5 = tf.layers.batch_normalization(dense1, center = False, scale = False, training = is_training)
+    #dropout1 = tf.layers.dropout(ba5, training = is_training)
+    dense2 = tf.layers.dense(ba5, units = 128, activation = tf.nn.relu)
+    ba6 = tf.layers.batch_normalization(dense2, center = False, scale = False, training = is_training)
+    #dropout2 = tf.layers.dropout(ba6, training = is_training)
+
+    net = tf.layers.dense(ba6, units = 10)
+    ############################################################################
+    #                            END OF YOUR CODE                              #
+    ############################################################################
+    return net
+
+
+learning_rate = 5e-3
+
+
+def optimizer_init_fn():
+    optimizer = None
+    ############################################################################
+    # TODO: Construct an optimizer that performs well on CIFAR-10              #
+    ############################################################################
+    #learning_rate = tf.train.exponential_decay(starter_learning_rate, global_step,
+    #                                           100000, 0.96, staircase=True)
+    optimizer = tf.train.RMSPropOptimizer(1e-3, decay = 0.9, momentum=0.1)
+    #optimizer = tf.train.MomentumOptimizer(learning_rate, momentum=0.9)
+    ############################################################################
+    #                            END OF YOUR CODE                              #
+    ############################################################################
+    return optimizer
+
+def resnet_init_fn(inputs, is_training):
+    net = resnet_110(inputs)
+    return net
+#device = '/cpu:0'
+print_every = 300
+num_epochs = 50
+train_part34(model_init_fn, optimizer_init_fn, num_epochs)
+#train_part34(resnet_init_fn,optimizer_init_fn, num_epochs)
